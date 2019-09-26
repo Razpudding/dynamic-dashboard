efcccbe4eb6b1b7af1bc87e4771c6e2aefc18573
Laurens <laurens.aarnoudse@gmail.com>
2019-09-26 14:10:44 +0200

    Added data to data object Used v-for to dynamically create html elements

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Dashboard!'
+  	message: "Hello Dashboard",
+    answers: [
+    {
+        "voorkeuren": "Frontend",
+        "alcohol": "4",
+        "id": 0
+    },
+    {
+        "voorkeuren": "Frontend, Interaction Design, UX",
+        "alcohol": "5",
+        "id": 1
+    },
+    {
+        "voorkeuren": "Visual Design, Interaction Design, UX, Design Research",
+        "alcohol": "0",
+        "id": 2
+    },
+    {
+        "voorkeuren": "Visual Design, Frontend",
+        "alcohol": "1",
+        "id": 3
+    }
+   	]
   }
 })
